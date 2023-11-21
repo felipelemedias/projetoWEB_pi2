0284d6482f25e101d78e1d4667163667ff2339fb
Felipe Leme Dias <108645543+felipelemedias@users.noreply.github.com>
2023-11-20 23:43:32 -0300

    Add files via upload

---
+++ tfpi/src/main/java/com/FGRW/tfpi/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.FGRW.tfpi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permitir qualquer origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Headers permitidos
+    }
+}
---
+++ tfpi/src/main/java/com/FGRW/tfpi/resources/EstadosResources.java
@@ -0,0 +1,58 @@
+package com.FGRW.tfpi.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.FGRW.tfpi.entities.Estados;
+import com.FGRW.tfpi.services.EstadosServices;
+
+@RestController
+@RequestMapping(value="/estados")
+public class EstadosResources {
+	@Autowired
+	EstadosServices service;
+	
+	@GetMapping
+	public ResponseEntity<List<Estados>> findAll() {
+		List<Estados> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Estados> findById(@PathVariable String id) {
+		Estados obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estados> createEstados(@RequestBody Estados Estados) {
+	    Estados savedEstados = service.insert(Estados);
+	    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedEstados.getId()).toUri();
+		return ResponseEntity.created(uri).body(savedEstados);
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Estados> updateEstados(@PathVariable String id, @RequestBody Estados updatedEstados) {
+	    Estados updated = service.update(id, updatedEstados);
+	    return ResponseEntity.ok().body(updated);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> deletes(@PathVariable String id) {
+	    service.delete(id);
+	    return ResponseEntity.noContent().build();
+	}
+}
